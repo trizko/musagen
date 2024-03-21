7c1b394f3c7ffbdbef9d0a05dc73cd8b187a1fa3
Tony Rizko <tony@rizkocircle.com>
2024-03-20 22:21:56 -0700

    Update to accept flags instead of positional cli args for midi and sf

--- src/midi_to_wav.py
+++ src/midi_to_wav.py
@@ -7,9 +7,9 @@ def midi_to_wav(midi_file, soundfont_file, output_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert MIDI to WAV')
-    parser.add_argument('midi_file', help='Input MIDI file')
-    parser.add_argument('soundfont_file', help='SoundFont file')
+    parser.add_argument('--midi', '-m', required=True, help='Input MIDI file')
+    parser.add_argument('--soundfont', '-s', required=True, help='SoundFont file')
     parser.add_argument('output_file', help='Output WAV file')
     args = parser.parse_args()
 
-    midi_to_wav(args.midi_file, args.soundfont_file, args.output_file)
+    midi_to_wav(args.midi, args.soundfont, args.output_file)
