66afc4fae073f15dc4212573dacd698c0fb417cd
Tony Rizko <tony@rizkocircle.com>
2024-03-21 23:03:08 -0700

    Add defaults to command line flags and args

--- src/midi_to_wav.py
+++ src/midi_to_wav.py
@@ -3,13 +3,14 @@ from midi2audio import FluidSynth
 
 def midi_to_wav(midi_file, soundfont_file, output_file):
     fs = FluidSynth(soundfont_file)
+
     fs.midi_to_audio(midi_file, output_file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert MIDI to WAV')
     parser.add_argument('--midi', '-m', required=True, help='Input MIDI file')
-    parser.add_argument('--soundfont', '-s', required=True, help='SoundFont file')
-    parser.add_argument('output_file', help='Output WAV file')
+    parser.add_argument('--soundfont', '-s', help='SoundFont file (defaults to fluidsynth `default.sf2`)', default='')
+    parser.add_argument('output_file', nargs='?', help='Output WAV file (defaults `to output.wav`)', default='output.wav')
     args = parser.parse_args()
 
     midi_to_wav(args.midi, args.soundfont, args.output_file)
