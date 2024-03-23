6c2179c68c4ba353da9cca7be0477cacc7eb42bb
Tony Rizko <tony@rizkocircle.com>
2024-03-22 23:01:20 -0700

    Create streamlit interface for midi file transform

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
+from midi_to_wav import midi_to_wav
+
 import streamlit as st
 
-# Add a title
-st.title("My First Streamlit App")
+st.title("Musagen")
 
-# Add some text
-st.write("Hello, World!")
+midi = st.file_uploader("MIDI file", type=["mid", "midi"], accept_multiple_files=False)
+soundfont = st.file_uploader("SoundFont file (optional)", type=["sf2"], accept_multiple_files=False)
 
-# Add an input field
-name = st.text_input("What's your name?", "Type your name here...")
-
-# Display the user's input
-if name:
-    st.write(f"Hello, {name}!")
+if st.button("Generate"):
+    if midi is None:
+        st.error("Please upload a MIDI file")
+    else:
+        midi_filename = "input.mid"
+        with open(midi_filename, "wb") as f:
+            f.write(midi.getbuffer())
+        if soundfont is not None:
+            soundfont_filename = "soundfont.sf2"
+            with open(soundfont_filename, "wb") as f:
+                f.write(soundfont.getbuffer())
+        else:
+            soundfont_filename = ""
+        midi_to_wav(midi_filename, soundfont_filename, "output.wav")
+        st.audio("output.wav", format="audio/wav")
