f68053ad8f67d97ae43942f7cccc5173e55110b2
Tony Rizko <tony@rizkocircle.com>
2024-04-09 20:46:53 -0700

    Update main script to run basic-pitch prediciton on audio

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from utils import midi_to_wav
+from basic_pitch.inference import predict
+from basic_pitch import ICASSP_2022_MODEL_PATH
 
 import streamlit as st
 
-st.title("Musagen")
+st.title("Loopy")
 
-midi = st.file_uploader("MIDI file", type=["mid", "midi"], accept_multiple_files=False)
-soundfont = st.file_uploader("SoundFont file (optional)", type=["sf2"], accept_multiple_files=False)
+midi = st.file_uploader("Audio file", type=["wav"], accept_multiple_files=False)
 
 if st.button("Generate"):
     if midi is None:
@@ -14,11 +14,5 @@ if st.button("Generate"):
         midi_filename = "input.mid"
         with open(midi_filename, "wb") as f:
             f.write(midi.getbuffer())
-        if soundfont is not None:
-            soundfont_filename = "soundfont.sf2"
-            with open(soundfont_filename, "wb") as f:
-                f.write(soundfont.getbuffer())
-        else:
-            soundfont_filename = ""
-        midi_to_wav(midi_filename, soundfont_filename, "output.wav")
-        st.audio("output.wav", format="audio/wav")
+        model_output, midi_data, note_events = predict(midi_filename, ICASSP_2022_MODEL_PATH)
+        st.write(model_output)
